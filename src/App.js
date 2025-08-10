1eaddd71b03026c46d8fab7ff2abcef866545b99
Зарина Колыбаева <zarinakolybaeva@MacBook-Air-Zarina.local>
2025-08-10 20:57:50 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Content from './components/Profile/Content.js';
+import Header from  './components/Header/Header.js';
+import SideBar from './components/SideBar/SideBar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="wrapper">
+    <Header/>
+    <SideBar/>
+    <Content/>
+</div>
+
   );
 }
+
+
 
 export default App;
